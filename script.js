a4e56af86f35889adcf2cd3e84793361b26089f3
DimaShapoval <101992246+DimaShapoval@users.noreply.github.com>
2022-05-22 20:50:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,41 +3,35 @@ const registrationImage = document.getElementById('planet-img');
 const textToChange = document.getElementById('text-to-change');
 const lables = document.querySelectorAll('lable');
 const options = document.querySelectorAll('option');
-
+const button = document.getElementById('button');
+let age;
 
 selectEl.addEventListener('change', function(){
-  //   if(this.value === 'mercury'){
-  //     registrationImage.src="./styles/Planet.svg";
-  //   }
-  //   else if(this.value === 'venus'){
-  //     registrationImage.src='./styles/Venus.png';
-  //   }
-  //   else if(this.value === 'earth'){
-  //     registrationImage.src='./styles/Earth.png';
-  //   }
-  //   else if(this.value === 'mars'){
-  //     registrationImage.src='./styles/Mars.png';
-  //   }
-  //   else if(this.value === 'neptune'){
-  //     registrationImage.src='./styles/Neptune.png';
-  //   }
-  // else{
-  //   registrationImage.src='./styles/Uran.svg';
-  //}
+  
   switch(true){
-      case this.value === 'mercury': registrationImage.src="./styles/Planet.svg", document.body.style.backgroundImage = "url(./styles/bg.jpg)",lableToChange(this);
+      case this.value === 'mercury': registrationImage.src="./styles/Planet.svg",lableToChange(this);
       break;
-      case this.value === 'venus': registrationImage.src='./styles/Venus.png', document.body.style.backgroundImage = "url(./styles/bg.jpg)",lableToChange(this);
+      case this.value === 'venus': registrationImage.src='./styles/Venus.png', lableToChange(this);
       break; 
-      case this.value === 'earth': registrationImage.src='./styles/Earth.png', document.body.style.backgroundImage = "url(./styles/bg.jpg)",lableToChange(this);
+      case this.value === 'earth': registrationImage.src='./styles/Earth.png', lableToChange(this);
       break;
-      case this.value === 'mars': registrationImage.src='./styles/Mars.png', document.body.style.backgroundImage = "url(./styles/bg.jpg)",lableToChange(this);
+      case this.value === 'mars': registrationImage.src='./styles/Mars.png', lableToChange(this);
       break;
-      case this.value === 'neptune': registrationImage.src='./styles/Neptune.png', document.body.style.backgroundImage = "url(./styles/bg.jpg)",lableToChange(this);
+      case this.value === 'neptune': registrationImage.src='./styles/Neptune.png', lableToChange(this);
       break;
-      case this.value === 'uranus': registrationImage.src='./styles/Uran.svg', document.body.style.backgroundImage = "url(./styles/bg.jpg)",lableToChange(this);
+      case this.value === 'uranus': registrationImage.src='./styles/Uran.svg', lableToChange(this);
       break;  
-      default: setSneakyRicardo(this);
+      default: age = prompt("Сколько вам лет?");
+      if(age<0){
+        alert("Такого возрвста не сущетвует")
+      }
+      else if(age<18 && age>0){
+        alert("Вам ещё нельзя такое видеть");
+      }
+       else{
+        setSneakyRicardo(this);
+      }
+     
       break;
   }
 })
@@ -51,5 +45,11 @@ function setSneakyRicardo(elem){
   document.body.style.backgroundImage = "url(./styles/DiscoHall.jpg)";
 }
 function lableToChange(elem){
+  textToChange.innerText = "полёта в космос";
+  registrationImage.style.transform="scale(1,1)";
   lables[2].innerText="Планета";
+  options[6].innerText = "Юпитер";
+  document.body.style.backgroundImage = "url(./styles/bg.jpg)"
+  
+  
 }
